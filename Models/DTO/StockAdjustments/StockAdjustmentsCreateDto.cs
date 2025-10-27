7bdc6238a4be6c81ef23304d966b22175a8c4602
Ryan Santoyo <ryansantoyo3@gmail.com>
2025-10-27 13:21:55 +0800

    StockAdjustmentCreateDto

--- Models/DTO/StockAdjustments/StockAdjustmentsCreateDto.cs
+++ Models/DTO/StockAdjustments/StockAdjustmentsCreateDto.cs
@@ -8,6 +8,7 @@ namespace katsuCMS_backend.Models.DTO.StockAdjustments
     public class StockAdjustmentsCreateDto
     {
         public int InventoryStockId { get; set; }
+        public string AdjustmentType { get; set; } = string.Empty;
         public decimal AdjustedQuantity { get; set; }
         public string Reason { get; set; } = string.Empty;
     }
